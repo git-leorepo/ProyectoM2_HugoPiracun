0ca74d9ebb0ae5a6a83635053b90eb35eee68e7b
HugoP <leogitrepo@gmail.com>
2026-06-05 23:19:09 -0500

    feat[index.js]: se agregaron los endpoint GET y POST para authors

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ const app = express();
 //traigo la configuracion desde el archivo config.js en la carpeta config
 const config = require('./config/config');
 
-
+//middleware para parsear el cuerpo de las solicitudes como JSON
 app.use(express.json());
+
+//Array para almacenar autores en memoria
+const authors = [];
+let nextId = 1;
 
 //health endpoint para verificar que la API esta funcionando correctamente
 app.get("/health", (req, res) => {
@@ -25,7 +29,46 @@ app.get("/health", (req, res) => {
     })
 })
 
+
+//Consultar todos los autores
+
+app.get("/authors", (req, res) => {
+    res.status(200).json(authors);
+});
+
+//Crear Authors
+app.post("/authors", (req, res) =>{
+    const {name, email, bio, created_at} = req.body;
+    //Validacion de campos requeridos
+    if(!name || !email){
+        return res.status(400).json({
+            error: "name and email are required"
+        })
+    }
+
+    const emailExists = authors.some(author => author.email.toLowerCase() === email.toLowerCase());
+
+    if (emailExists) {
+        return res.status(409).json({ // 409 es el código HTTP correcto para Conflictos
+            error: "An author with this email already exists"
+        });
+    }
+    //Crear el objeto autor
+    const newAuthor = {
+        id: nextId++,
+        name,
+        email,
+        bio: bio || "",
+        created_at: created_at || new Date().toISOString()
+    }
+    authors.push(newAuthor);
+
+    res.status(201).json(newAuthor);
+})
+
+
+
 //comprobacion que el puerto esta funcionando
 app.listen(config.PORT, ()=>{
-    console.log(`Servidor corriendo en el puerto ${config.PORT} en modo ${config.NODE_ENV}`);
+    console.log(`Servidor corriendo en el puerto http://localhost:${config.PORT} en modo ${config.NODE_ENV}`);
 })

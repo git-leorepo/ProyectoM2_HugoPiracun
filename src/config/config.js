8bcf469cdacd53ed1ddab3fd793e647c298aa03e
HugoP <leogitrepo@gmail.com>
2026-06-26 19:03:46 -0500

    feat: se añaden funcionalidades al proyecto

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export default {
-    PORT: process.env.PORT || 3001,
+    PORT: process.env.PORT || 3000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || 'mongodb://localhost:27017/myapp',
     API_KEY: process.env.API_KEY,
